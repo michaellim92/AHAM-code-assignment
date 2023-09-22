4cb537c7e9daefdd9e276d696142e6f7db2d45d5
michael lim <michael.limnh@gmail.com>
2023-09-22 11:24:58 +0800

    Add fund card and fund view skeleton

---
+++ src/components/FundCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import {ref} from 'vue'
+</script>
+
+<template>
+    <div class="fund-card__wrapper">
+        <div class="fund-card__header">
+            <div class="fund-card__title">
+                <!-- fund title -->
+            </div>
+        </div>
+        <div class="fund-card__body">
+            <div class="fund-card__details">
+                <!-- details (name, description, investment type, performance, current asset value) -->
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/FundsView.vue
@@ -0,0 +1,9 @@
+<script>
+    import {ref} from 'vue';
+</script>
+
+<template>
+    <div class="fund-view__wrapper">
+        <!-- should loop through data and show FundCard for each -->
+    </div>
+</template>
